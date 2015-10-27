9dfe49ccf86a8c9db471ee9b8fa247b46c5c4f75
rgordonatrsgc <rgordon@rsgc.on.ca>
2015-10-27 13:42:59 +0000

    Thought a change to welcome message would be nice.

--- index.php
+++ index.php
@@ -17,6 +17,6 @@
 
 <body>
   <script src="js/scripts.js"></script>
-  <p>Welcome to the Royal St. George's Library website!</p>
+  <p>Welcome to the Royal St. George's Library website! We welcome everyone.</p>
 </body>
 </html>
